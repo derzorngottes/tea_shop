1fae5f99c81c167261a051519f23f9bf55351988
derzorngottes <amy.conchie@gmail.com>
2016-05-17 00:31:52 -0700

    Category for tea dropdown

--- app/teas/Teas.controller.js
+++ app/teas/Teas.controller.js
@@ -10,6 +10,16 @@
   function Teas(TeaService) {
     var vm = this;
 
+    vm.teaCats = [
+      'indian',
+      'japanese',
+      'chinese',
+      'green',
+      'black',
+      'white',
+      'rooibos'
+    ];
+
     TeaService.getTeas().then(function(teas) {
       vm.teas = teas.data;
     })
